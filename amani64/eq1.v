6a83d7ecf74cf1c6ab071553f9bd8cc72d07d362
Gustavo Romero <gromero@br.ibm.com>
2016-11-27 17:46:42 -0200

    Add four registers to keep data

--- amani64/eq1.v
+++ amani64/eq1.v
@@ -1,20 +1,37 @@
-module eq1 
-(
-	input wire i0, i1, arduino_input,
-	output wire eq, cpld_output
-);
+`define D_SIZE 31
 
-// signal declaration
-wire p0, p1;
+module eq1(
+          input wire  [1:0] addr,
+          output wire [`D_SIZE:0] data,
+          input wire rw,
+          input wire clk
+         );
 
-// body
-// sum of two product terms
-assign eq = p0 | p1;
-// product terms
-assign p0 = ~i0 & ~i1;
-assign p1 = i0 & i1;
+         reg [`D_SIZE:0] r0;     // Register r0
+         reg [`D_SIZE:0] r1;     // Register r1
+         reg [`D_SIZE:0] r2;     // Register r2
+         reg [`D_SIZE:0] r3;     // Register r3
+         reg [`D_SIZE:0] i_data; // Output register
 
-// follow Arduino blink led
-assign cpld_output = arduino_input;
+         always @(negedge clk)
+         begin // always block
 
+         if (rw == 1'b0) // rw = 0 (set memory content)
+           i_data = (addr == 2'b00) ? r0 :
+                    (addr == 2'b01) ? r1 :
+                    (addr == 2'b10) ? r2 : r3;
+
+         else            // rw = 1 (get memory content)
+           if (addr == 2'b00)
+             r0 = data;
+           else if (addr == 2'b01)
+             r1 = data;
+           else if (addr == 2'b10)
+             r2 = data;
+           else          // addr == 11
+             r3 = data;
+
+         end // always block
+
+          assign data = i_data;
 endmodule
