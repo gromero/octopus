bf7848a2543cf2fb42e7b9513366e57ff37c7409
Gustavo Romero <gromero@br.ibm.com>
2016-11-29 23:57:20 -0200

    Change module interface totally
    Change eq1 module interface totally
    Use case instead of if/else for cleannes
    Add a chip enable pin to control data impedance

--- amani64/eq1.v
+++ amani64/eq1.v
@@ -1,37 +1,36 @@
-`define D_SIZE 31
+`define D_SIZE 7
+`define A_SIZE 1
 
-module eq1(
-          input wire  [1:0] addr,
-          output wire [`D_SIZE:0] data,
-          input wire rw,
-          input wire clk
-         );
+module eq1(data,address,clock,rw,ce);
 
-         reg [`D_SIZE:0] r0;     // Register r0
-         reg [`D_SIZE:0] r1;     // Register r1
-         reg [`D_SIZE:0] r2;     // Register r2
-         reg [`D_SIZE:0] r3;     // Register r3
-         reg [`D_SIZE:0] i_data; // Output register
+inout wire [`D_SIZE:0] data;
+input wire [`A_SIZE:0] address;
+input wire rw, ce, clock;
 
-         always @(negedge clk)
-         begin // always block
+// Registers
+reg [`D_SIZE:0] r0, r1, r2, r3;  // GPR
+reg [`D_SIZE:0] cr;              // Current register on bus
 
-         if (rw == 1'b0) // rw = 0 (set memory content)
-           i_data = (addr == 2'b00) ? r0 :
-                    (addr == 2'b01) ? r1 :
-                    (addr == 2'b10) ? r2 : r3;
+always @(negedge clock)
+  begin
+    case ({ce, rw, address}) // 0 => read, 1 => write
 
-         else            // rw = 1 (get memory content)
-           if (addr == 2'b00)
-             r0 = data;
-           else if (addr == 2'b01)
-             r1 = data;
-           else if (addr == 2'b10)
-             r2 = data;
-           else          // addr == 11
-             r3 = data;
+	         // Getter
+                 4'b1000:  cr = r0;
+		 4'b1001:  cr = r1;
+		 4'b1010:  cr = r2;
+		 4'b1011:  cr = r3;
+		 // Setter
+		 4'b1100:  r0   = data;
+		 4'b1101:  r1   = data;
+		 4'b1110:  r2   = data;
+		 4'b1111:  r3   = data;
 
-         end // always block
+                 // ce = 0, data is just ´z´
+		 default: cr = 8'bzzzzzzzz;
+    endcase
+  end
 
-          assign data = i_data;
+ assign data = cr;
+
 endmodule
